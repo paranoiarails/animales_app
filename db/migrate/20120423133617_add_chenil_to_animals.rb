5160baa107275599b7a00824c6eb594eff62c9fe
Juanjo <Juanrodriguez110@hotmail.com>
2012-07-19 20:30:11 +0200

    buscador updating sqlite3

--- db/migrate/20120423133617_add_chenil_to_animals.rb
+++
@@ -1,6 +0,0 @@
-class AddChenilToAnimals < ActiveRecord::Migration
-  def change
-    add_column :animals, :chenil_id, :integer
-
-  end
-end
--- db/migrate/20120705163422_create_pers.rb
+++
@@ -1,9 +0,0 @@
-class CreatePers < ActiveRecord::Migration
-  def change
-    create_table :pers do |t|
-      t.string :nombre
-
-      t.timestamps
-    end
-  end
-end
